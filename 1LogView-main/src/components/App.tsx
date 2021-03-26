af06f72f8a95011191efc23749d132356ee0ae18
Sahil Batra <sahilbatra1211@gmail.com>
2021-03-26 11:34:30 +0530

    request api in proper format

--- 1LogView-main/src/components/App.tsx
+++ 1LogView-main/src/components/App.tsx
@@ -7,6 +7,7 @@ import '../assets/scss/App.scss';
 import { resultsData } from '../mockData/mockData'
 import LogData from './logData/LogData';
 import Navbar from './nav/Navbar';
+import moment from 'moment';
 
 
 const verticalGapStackTokens2: IStackTokens = {
@@ -23,11 +24,10 @@ const VerticalStackBasicExample: React.FunctionComponent = () => {
 
   const [resultsFetched, setResultsFetched] = useState(false);
   const [logData, setLogData] = useState(resultsData);
-  const [startTime, setStartTime] = useState("");
-  const [endTime, setEndTime] = useState("");
+  const [startTime, setStartTime] = useState(moment().format());
+  const [endTime, setEndTime] = useState(moment().format());
 
   const [reqIDFieldValue, setReqIDFieldValue] = React.useState('');
-
   const onChangeReqIDFieldValue = React.useCallback(
     (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
       setReqIDFieldValue(newValue || '');
@@ -36,11 +36,13 @@ const VerticalStackBasicExample: React.FunctionComponent = () => {
   );
 
   const onChangeStartDateTime=(date)=>{
-    setStartTime(date);
+    
+    setStartTime(date.format());
   }
 
   const onChangeEndDateTime=(date)=>{
-    setEndTime(date);
+    
+    setEndTime(date.format());
   }
 
   function _onClick() {
@@ -51,10 +53,9 @@ const VerticalStackBasicExample: React.FunctionComponent = () => {
 
     const currDate = new Date();
 
-    const startDate = addMinutes(currDate, -15);
-    const endDate = addMinutes(currDate, 15);
-
-    console.log(`https://localhost:44388/api/Logs/${reqIDFieldValue}?startDate=${startDate.toISOString()}&endDate=${endDate.toISOString()}`);
+  //  const startDate = addMinutes(currDate, -15);
+   // const endDate = addMinutes(currDate, 15);
+    console.log(`https://localhost:44388/api/Logs/${reqIDFieldValue}?startDate=${startTime}&endDate=${endTime}`);
 
     // https://localhost:44388/api/Logs/0ac1a787-b133-4cb9-8fc8-d9b1db9f323f?
     // startDate=2020-11-10T14:53:02.148Z&endDate=2020-11-10T15:23:02.148Z
@@ -66,6 +67,7 @@ const VerticalStackBasicExample: React.FunctionComponent = () => {
     fetch(`https://jsonplaceholder.typicode.com/todos/1`)
       .then(response => response.json())
       .then(resultsData => {
+        console.log(resultsData);
         setResultsFetched(true);
       });
   }
